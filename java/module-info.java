2f1fa90e4a5ba46ef7a657d44f6b2cd5bb031e21
Kuba Bed <101566833+Zydson123@users.noreply.github.com>
2023-03-07 11:21:04 +0100

    Delete java directory

--- java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.chessjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.unsupported;
-
-
-    opens com.example.chessjavafx to javafx.fxml;
-    exports com.example.chessjavafx;
-}
